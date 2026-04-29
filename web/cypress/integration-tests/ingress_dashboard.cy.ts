d54c8f1317c1f18928349ab0c95c1996fa7968ed
Amoghrd <aramesha@redhat.com>
2026-04-29 16:35:00 -0400

    Update IDMS

--- web/cypress/integration-tests/ingress_dashboard.cy.ts
+++ web/cypress/integration-tests/ingress_dashboard.cy.ts
@@ -24,24 +24,20 @@ describe('Network_Observability networking dashboards tests', { tags: ['Network_
         dashboard.visit()
         cy.visit(`/monitoring/dashboards/grafana-dashboard-ingress-operator`)
 
-        cy.get('[data-test="poll-interval-dropdown"] > .pf-v6-c-menu-toggle').should('exist').then(btn => {
-            cy.wrap(btn).click().then(drop => {
-                cy.contains('15 seconds').should('exist').click()
-            })
-        })
+        // Set refresh interval to 15 seconds
+        cy.get('#refresh-interval-dropdown-dropdown').should('exist').click()
+        cy.contains('15 seconds').should('exist').click()
 
-        cy.get('[data-test="time-range-dropdown"]').should('exist').then(btn => {
-            cy.wrap(btn).click().then(drop => {
-                cy.contains('Last 5 minutes').should('exist').click()
-            })
-        })
+        // Set time range to Last 5 minutes
+        cy.get('#monitoring-time-range-dropdown-dropdown').should('exist').click()
+        cy.contains('Last 5 minutes').should('exist').click()
 
         cy.wait(1000)
         cy.get('#content-scrollable').scrollTo('bottom')
         cy.wait(1000)
     })
 
-    it('(OCP-69946, aramesha, Network_Observability), should have ingress operator dashboards', function () {
+    it('(OCP-69946, aramesha), should have ingress operator dashboards', function () {
         // verify that 'Current Total Incoming Bandwidth', 'Current Total Outgoing Bandwidth', 'HTTP Error Rate' and 'HTTP Server Average Response Latency' panels exist and are populated
         cy.checkDashboards(ingressPanels)
 
